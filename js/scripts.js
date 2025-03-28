a0c0f156f77682b86ded223927d75c2e822587e4
macarena <macarena.c.rios@gmail.com>
2025-03-28 10:56:03 +0100

    ejer-extra25

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,50 @@
 // 1️⃣ Macarena y la validación de contraseñas: Crea una función validatePassword que reciba un objeto con una propiedad password (string) y devuelva un objeto con las propiedades isValid (boolean) y errors (array de strings) que indiquen si la contraseña cumple con los siguientes criterios:
 
-const validatePassword = password => {
-  const checkPassword = {
-    isValid: [],
+const validatePassword = data => {
+  const result = {
+    isValid: false,
     errors: []
   };
 
-  if (password.length < 8) {
-    checkPassword.errors.push('Password must be at least 8 characters long.');
+  if (data.password.length < 8) {
+    result.errors.push('Password must be at least 8 characters long.');
   }
-  if (!/[A-Z]/.test(password)) {
-    checkPassword.errors.push(
-      'Password must contain at least one uppercase letter.'
-    );
+  if (!/[A-Z]/.test(data.password)) {
+    result.errors.push('Password must contain at least one uppercase letter.');
   }
-  if (!/[a-z]/.test(password)) {
-    checkPassword.errors.push(
-      'Password must contain at least one lowercase letter.'
-    );
+  if (!/[a-z]/.test(data.password)) {
+    result.errors.push('Password must contain at least one lowercase letter.');
   }
-  if (!/\d/.test(password)) {
-    checkPassword.errors.push('Password must contain at least one number.');
-  }
-  if (checkPassword.errors.length === 0) {
-    checkPassword.isValid.push('valid password');
+  if (!/\d/.test(data.password)) {
+    result.errors.push('Password must contain at least one number.');
   }
 
-  console.log(checkPassword);
+  result.isValid = result.errors.length === 0;
+
+  console.log(result);
 };
 
-validatePassword('invalid');
-validatePassword('Estasiesvalida2');
+validatePassword({ password: 'abc123' });
+validatePassword({ password: 'Adfdfdabc123' });
 
-// 2️⃣ Sabrina y el sistema de evaluación de estudiantes: Crea una función evaluateStudents que reciba un objeto que representa una asignatura (subject). Este objeto debe tener las propiedades: subjectName: El nombre de la asignatura (string). teacher: El nombre del profesor a cargo (string). students: Un objeto que contiene a los estudiantes como claves y sus notas (grades) como valores. La función debe imprimir un objeto con la información de los estudiantes aprobados (note >= 5) junto con el nombre de la asignatura y el profesor.
+// 3️⃣ Abby y la traducción de palabras: Crea una función translateWords que reciba un objeto translationTask con:
 
-const evaluateStudents = () => {
-  const subject = {
-    subjectname: 'Web Development',
-    teacher: 'Mr. Dios',
-    students: {
-      Macarena: 8,
-      Bego: 4,
-      Abby: 6,
-      Camila: 3,
-      Sabrina: 5
-    }
+const translateWords = translationTask => {
+  const result = {
+    projectName: translationTask.projectName,
+    translator: translationTask.translator,
+    translated: []
   };
+
+  for (const word of translationTask.words) {
+    console.log(translationTask.dictionary[word]);
+  }
 };
+
+translateWords({
+  // reciba, puedes poner el objeto cuando llama a la función.
+  projectName: 'Basic Translator',
+  translator: 'Abby',
+  words: ['hello', 'world'],
+  dictionary: { hello: 'hola', world: 'mundo' }
+});
